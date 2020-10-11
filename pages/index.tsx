da7c7926c9dac32c3c423ab4236be75091fe96a0
Phillip Maier <pmaier983@gmail.com>
2020-10-11 18:07:39 -0400

    Setup TS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>Had to put a man on pause</h1>
       <Link href="/blog"><a>Go To Blog</a></Link>
     </div>
   )
