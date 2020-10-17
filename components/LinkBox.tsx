b5a6d523c5b1fd0e6608b2e330410534f0784878
Phillip Maier <pmaier983@gmail.com>
2020-10-16 21:32:09 -0400

    Add git link box

---
+++ components/LinkBox.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import styled, { css } from "styled-components"
+
+interface StyledContainerProps {
+  color: string
+  transition?: boolean
+  size: string
+}
+
+const StyledContainer = styled.div<StyledContainerProps>`
+  ${({ color, size }) => css`
+    height: ${size};
+    width: ${size};
+    border: 2px solid ${color};
+    background-color: ${color};
+  `}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  transition: opacity 0.5s ease-in-out;
+  :hover {
+    opacity: ${({ transition }) => (transition ? 0 : 1)};
+    transition: opacity 0.5s ease-in-out;
+  }
+  :active {
+    opacity: ${({ transition }) => (transition ? 0 : 1)};
+    transition: opacity 0.5s ease-in-out;
+  }
+`
+
+type LinkBox = StyledContainerProps
+
+export const LinkBox: React.FC<LinkBox> = ({ children, ...rest }) => (
+  <StyledContainer {...rest} transition={!children}>
+    {children}
+  </StyledContainer>
+)
