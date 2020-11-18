39bb28cf234a2d23dd3d448a04c9f6a2c20dd2f7
Phillip Maier <pmaier983@gmail.com>
2020-11-17 21:26:17 -0500

    Return StyleSheet to head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,10 @@ const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+        rel="stylesheet"
+      ></link>
     </Head>
     <AlertProvider>
       <ThemeWrapper>
