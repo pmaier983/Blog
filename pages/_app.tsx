0b976c282a90a3ff689d400e348ad1e9f71de2c4
Phillip Maier <pmaier983@gmail.com>
2020-11-17 21:36:22 -0500

    Remove viewport meta tag, auto works better

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import AlertProvider from "../components/contexts/AlertProvider"
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link
         href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
         rel="stylesheet"
