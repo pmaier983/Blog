381cd6b420eca74a69fb680ec376a4479a64d503
Phillip ED Maier <pmaier983@gmail.com>
2024-01-26 20:55:20 -0500

    Add React & Eslint?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from "@astrojs/sitemap"
-import stylex from "./src/stylex-integration"
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), stylex()],
-})
+  integrations: [mdx(), sitemap(), tailwind(), react()]
+});
