43eb23af1ed7110578e2d55a4a6c98e743f65c0d
Phillip ED Maier <pmaier983@gmail.com>
2024-01-30 21:30:35 -0500

    Add Icons

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,10 @@
 export const SITE_TITLE = "Phillip Maier's Personal Website"
 export const SITE_DESCRIPTION =
   "A place to learn about Phillip, and a place for Phillip to experiment."
+
+export const ICONS = {
+  menu: "menu",
+  "refresh-cw": "refresh-cw",
+}
+
+export type Icon = keyof typeof ICONS
