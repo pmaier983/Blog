49f02a797433e5b24d075d578a76383df5282e78
Phillip ED Maier <pmaier983@gmail.com>
2024-02-03 16:28:05 -0500

    Add a close Icon

--- src/consts.ts
+++ src/consts.ts
@@ -5,9 +5,11 @@ export const SITE_TITLE = "Phillip Maier's Personal Website"
 export const SITE_DESCRIPTION =
   "A place to learn about Phillip, and a place for Phillip to experiment."
 
+// ITTT: Add a new icon to the ICONS object & icons.svg together!
 export const ICONS = {
   menu: "menu",
   "refresh-cw": "refresh-cw",
+  close: "close",
 }
 
 export type Icon = keyof typeof ICONS
