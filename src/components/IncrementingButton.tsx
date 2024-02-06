0935abd8d9a15220dd5dcfd8ca845750541e2f29
Phillip ED Maier <pmaier983@gmail.com>
2024-02-05 22:01:00 -0500

    Prepare for incrementing button

---
+++ src/components/IncrementingButton.tsx
@@ -0,0 +1,7 @@
+export const IncrementingButton = () => {
+  return (
+    <button className="bg-green-600 h-10 p-2 rounded-md" onClick={() => {}}>
+      Click me to increment a counter!
+    </button>
+  )
+}
