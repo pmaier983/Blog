1cfd42c043ca308a1eb3cb7c962e854a9e24940f
Phillip ED Maier <pmaier983@gmail.com>
2024-02-19 13:26:55 -0500

    Record information on each button click

--- apps/blog-backend/src/api/buttons/incrementButton.ts
+++ apps/blog-backend/src/api/buttons/incrementButton.ts
@@ -2,36 +2,53 @@ import { eq } from "drizzle-orm"
 import type { NextFunction, Request, Response } from "express"
 import { z } from "zod"
 import { db } from "~/db"
-import { buttons } from "~/db/schema"
+import { buttonClicks, buttons } from "~/db/schema"
 import { getRequestParams } from "~/utils"
 
 const incrementButtonSchema = z.object({
   params: z.object({
     name: z.string(),
+    userAgent: z.string().optional(),
+    language: z.string().optional(),
+    platform: z.string().optional(),
+    screenResolution: z.string().optional(),
   }),
 })
 
 const V1 = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const {
-      params: { name },
-    } = await getRequestParams({ schema: incrementButtonSchema, req, next })
+    const { params } = await getRequestParams({
+      schema: incrementButtonSchema,
+      req,
+      next,
+    })
 
     await db.transaction(async (trx) => {
       const button = await trx.query.buttons.findFirst({
-        where: eq(buttons.name, name),
+        where: eq(buttons.name, params.name),
       })
 
       if (!button) {
-        throw new Error(`No button was found with the name: ${name}`)
+        throw new Error(`No button was found with the name: ${params.name}`)
       }
+
+      await trx.insert(buttonClicks).values({
+        id: "",
+        buttonId: button.id,
+        timestamp: new Date().toISOString(),
+        // TODO: is there a way to avoid writing these nulls?
+        userAgent: params.userAgent ?? null,
+        language: params.language ?? null,
+        platform: params.platform ?? null,
+        screenResolution: params.screenResolution ?? null,
+      })
 
       await trx
         .update(buttons)
         .set({
           clickCount: button.clickCount + 1,
         })
-        .where(eq(buttons.name, name))
+        .where(eq(buttons.name, params.name))
     })
 
     return res.send(200)
--- apps/blog-backend/src/db/schema.ts
+++ apps/blog-backend/src/db/schema.ts
@@ -1,4 +1,6 @@
+import { relations } from "drizzle-orm"
 import { int, varchar, mysqlTableCreator } from "drizzle-orm/mysql-core"
+import { createId } from "@paralleldrive/cuid2"
 
 const mysqlTable = mysqlTableCreator((name) => `personal_site_${name}`)
 
@@ -6,4 +8,17 @@ export const buttons = mysqlTable("buttons", {
   id: varchar("id", { length: 191 }).primaryKey().notNull(),
   name: varchar("name", { length: 191 }).notNull(),
   clickCount: int("click_count").notNull().default(0),
+})
+export const buttonsRelations = relations(buttons, ({ many }) => ({
+  buttons: many(buttonClicks),
+}))
+
+export const buttonClicks = mysqlTable("button_clicks", {
+  id: varchar("id", { length: 191 }).primaryKey().notNull().default(createId()),
+  buttonId: varchar("button_id", { length: 191 }).notNull(),
+  timestamp: varchar("timestamp", { length: 191 }).notNull(),
+  userAgent: varchar("user_agent", { length: 512 }),
+  language: varchar("language", { length: 191 }),
+  platform: varchar("platform", { length: 191 }),
+  screenResolution: varchar("screen_resolution", { length: 191 }),
 })
