08b30f3c9e0ebed79484620f258da30cecf4e8db
Phillip ED Maier <pmaier983@gmail.com>
2024-05-07 20:02:41 -0400

    Get Astro build working

--- apps/astro-blog/astro.config.mjs
+++ apps/astro-blog/astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig, passthroughImageService } from "astro/config"
+import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), react()]
-});
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+})
