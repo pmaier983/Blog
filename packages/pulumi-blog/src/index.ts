75f2d015cdb992342e87bffdaf133565241fccf8
Phillip ED Maier <pmaier983@gmail.com>
2024-11-30 08:30:33 -0800

    Setup pulumi upload to GCR + start service

--- packages/pulumi-blog/src/index.ts
+++ packages/pulumi-blog/src/index.ts
@@ -1,128 +1,81 @@
 /**
  * Tutorials & Docs followed for the setup of this file:
+ *
+ * When I tried getting it to work on a VM.
  * - https://www.pulumi.com/registry/packages/gcp/how-to-guides/gce-webserver/
  * - https://github.com/pulumi/templates/blob/master/vm-gcp-typescript/index.ts
  * - https://gist.github.com/kurokobo/25e41503eb060fee8d8bec1dd859eff3
+ *
+ * When I tried getting it to work on Cloud Run.
+ * - https://cloud.google.com/artifact-registry/docs/docker/authentication
+ * - https://www.pulumi.com/registry/packages/gcp/how-to-guides/gcp-ts-docker-gcr-cloudrun/
+ * - https://github.com/pulumi/examples/blob/master/gcp-ts-docker-gcr-cloudrun/cloud-run-deploy/index.ts
+ * - https://github.com/pulumi/examples/blob/master/gcp-ts-cloudrun/index.ts
  */
 
 import * as gcp from "@pulumi/gcp"
 import * as pulumi from "@pulumi/pulumi"
+import * as docker from "@pulumi/docker"
 
 import { envSchema } from "./envSchema"
-import { execSync } from "child_process"
 
 const env = envSchema.parse(process.env)
 
-const instanceTag = "webserver"
-const SERVICE_PORT = "80"
+const imageName = "astro-blog"
 
-// Create a new network for the virtual machine.
-const network = new gcp.compute.Network("network", {
-  autoCreateSubnetworks: false,
-})
+/* ########## Upload the docker images to Google Cloud Registry. ########## */
 
-// Create a subnet on the network.
-const subnet = new gcp.compute.Subnetwork("subnet", {
-  ipCidrRange: "10.0.1.0/24",
-  network: network.id,
-})
-
-// Create a firewall allowing inbound access over ports 80 (for HTTP) and 22 (for SSH).
-const firewall = new gcp.compute.Firewall("firewall", {
-  network: network.selfLink,
-  allows: [
-    {
-      protocol: "tcp",
-      /**
-       * 22 - ssh port
-       * 80 - http port (service port?)
-       * 8080 - backend API port
-       * 4321 - astro frontend port
-       */
-      ports: ["22", SERVICE_PORT, "8080", "4321"],
-    },
-  ],
-  direction: "INGRESS",
-  sourceRanges: ["0.0.0.0/0"],
-  targetTags: [instanceTag],
-})
-
-const envContent = Object.entries(env)
-  .map(([key, value]) => `${key}=${value}`)
-  .join("\n")
-
-// Function to get the current Git branch
-const currentGitBranch = (() => {
-  try {
-    return execSync("git rev-parse --abbrev-ref HEAD").toString().trim()
-  } catch (error) {
-    throw new Error(
-      "Failed to detect Git branch. Make sure you are in a valid Git repository.",
-    )
-  }
-})()
-
-// TODO: ensure this clone works after merging in to main!
-// Define a script to be run when the VM starts up.
-const metadataStartupScript = `#!/bin/bash
-set -e
-
-WORK_DIR="/home/phillipmaier"
-mkdir -p $WORK_DIR
-cd $WORK_DIR
-
-# Clone the repository
-git clone --branch ${currentGitBranch} https://github.com/pmaier983/Blog.git
-cd Blog
-
-# Write the .env & .env.prod file
-cat <<EOF > .env.prod
-${envContent}
-EOF
-
-cat <<EOF > .env
-${envContent}
-EOF
-
-# Download Docker Compose
-curl -s https://gist.githubusercontent.com/kurokobo/25e41503eb060fee8d8bec1dd859eff3/raw/0d7cd29472f0eaa26ce424071456ad84b24fb318/installer.sh | bash
-source ~/.bashrc
-`
-
-// Create the virtual machine.
-const instance = new gcp.compute.Instance(
-  "instance",
-  {
-    name: env.INSTANCE_NAME,
-    zone: env.ZONE,
-    machineType: "e2-micro",
-    bootDisk: {
-      initializeParams: {
-        image: "cos-cloud/cos-117-18613-75-37",
-      },
-    },
-    networkInterfaces: [
-      {
-        network: network.id,
-        subnetwork: subnet.id,
-        accessConfigs: [{}],
-      },
-    ],
-    serviceAccount: {
-      scopes: ["https://www.googleapis.com/auth/cloud-platform"],
-    },
-    allowStoppingForUpdate: true,
-    metadataStartupScript,
-    tags: [instanceTag],
+const astroBlogImage = new docker.Image(imageName, {
+  imageName: pulumi.interpolate`gcr.io/${gcp.config.project}/${imageName}:latest`,
+  build: {
+    context: "../../",
+    dockerfile: "../../apps/astro-blog/Dockerfile",
+    platform: "linux/amd64",
+    args: env,
   },
-  { dependsOn: firewall },
-)
-
-const instanceIP = instance.networkInterfaces.apply((interfaces) => {
-  return interfaces?.[0]?.accessConfigs?.[0]?.natIp
 })
 
-// Export the instance's name, public IP address, and HTTP URL.
-export const name = instance.name
-export const ip = instanceIP
-export const url = pulumi.interpolate`http://${instanceIP}:${SERVICE_PORT}`
+// add a unique ID to ensure the service updates
+const serviceName = `${imageName}-service`
+const location = "us-west2"
+
+/* ########## Deploy the Docker image to Google Cloud Run. ########## */
+
+// Deploy to Cloud Run if there is a difference in the sha, denoted above.
+const astroService = new gcp.cloudrun.Service(serviceName, {
+  location,
+  name: serviceName,
+  template: {
+    spec: {
+      containers: [
+        {
+          image: astroBlogImage.imageName,
+          ports: [{ containerPort: 4321 }],
+          resources: {
+            limits: {
+              memory: "1Gi",
+            },
+          },
+          livenessProbe: {
+            httpGet: {
+              path: "/",
+              port: 4321,
+            },
+            initialDelaySeconds: 60, // Increase initial delay for health check
+            periodSeconds: 30, // Increase period for health check
+          },
+        },
+      ],
+    },
+  },
+})
+
+// Open the service to public unrestricted access
+const openAstroService = new gcp.cloudrun.IamMember(`${imageName}-everyone`, {
+  service: astroService.name,
+  location,
+  role: "roles/run.invoker",
+  member: "allUsers",
+})
+
+export const astroUrl = astroService?.statuses[0]?.url
