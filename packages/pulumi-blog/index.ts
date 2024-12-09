00497e6c8db13602986f6750a5ccd07361e746ea
Phillip ED Maier <pmaier983@gmail.com>
2024-12-08 18:54:38 -0800

    Mess with pulumi-docker way too much

--- packages/pulumi-blog/index.ts
+++ packages/pulumi-blog/index.ts
@@ -14,6 +14,7 @@
  */
 import * as gcp from "@pulumi/gcp"
 import * as pulumi from "@pulumi/pulumi"
+import path from "path"
 
 import { execSync } from "child_process"
 
@@ -34,19 +35,48 @@ const networkId = stackReference.getOutput("networkId")
 const subnetId = stackReference.getOutput("subnetId")
 const staticIpAddress = stackReference.getOutput("staticIpAddress")
 
-// const = z
+// TODO: get the docker deploy working!
+
+// The combined required env variables for both the docker images
+// const dockerEnvObject = z
 //   .object({
-//     networkId: z.string(),
-//     subnetId: z.string(),
-//     firewallId: z.string(),
-//     staticIpAddress: z.string(),
+//     DATABASE_URL: z.string(),
+//     SUPABASE_URL: z.string(),
+//     SUPABASE_ANON_KEY: z.string(),
+//     PUBLIC_BACKEND_API_URL: z.string(),
+//     PUBLIC_FRONTEND_URL: z.string(),
+//     PUBLIC_IN_NETWORK_BACKEND_API_URL: z.string(),
 //   })
-//   .parse({
-//     networkId: stackReference.getOutput("networkId"),
-//     subnetId: stackReference.getOutput("subnetId"),
-//     firewallId: stackReference.getOutput("firewallId"),
-//     staticIpAddress: stackReference.getOutput("staticIpAddress"),
-//   })
+//   .parse(process.env)
+
+/* ############### Deploy both docker images ###############*/
+
+// const astroImageName = `astro-blog`
+// const backendImageName = `blog-backend`
+
+// const backendImage = new docker.Image(backendImageName, {
+//   imageName: pulumi.interpolate`gcr.io/${PROJECT}/${backendImageName}:latest`,
+//   build: {
+//     context: "../../",
+//     dockerfile: "../../apps/blog-backend/Dockerfile",
+//     platform: "linux/amd64",
+//     args: dockerEnvObject,
+//   },
+// })
+
+// export const backendImageUrn = backendImage.urn
+
+// const astroImage = new docker.Image(astroImageName, {
+//   imageName: pulumi.interpolate`gcr.io/${PROJECT}/${astroImageName}:latest`,
+//   build: {
+//     context: "../../",
+//     dockerfile: "../../apps/astro-blog/Dockerfile",
+//     platform: "linux/amd64",
+//     args: dockerEnvObject,
+//   },
+// })
+
+// export const astroImageUrn = astroImage.urn
 
 const envContent = Object.entries(process.env)
   .map(([key, value]) => `${key}=${value}`)
