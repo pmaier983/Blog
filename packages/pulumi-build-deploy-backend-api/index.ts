9a53de2b7a22f9190838d6c06247c7435c48688b
Phillip ED Maier <pmaier983@gmail.com>
2025-07-06 15:00:10 -0700

    Add some helpful comments around pulumi
    Also add more comments about some old stuff

--- packages/pulumi-build-deploy-backend-api/index.ts
+++ packages/pulumi-build-deploy-backend-api/index.ts
@@ -15,6 +15,7 @@
  * When I got it working on Cloud Run.
  * - https://www.pulumi.com/docs/iac/using-pulumi/organizing-projects-stacks/
  * - https://www.pulumi.com/registry/packages/gcp/api-docs/cloudrunv2/service/
+ * - https://g.co/gemini/share/ff420751e075
  */
 
 import * as pulumi from "@pulumi/pulumi"
@@ -47,6 +48,10 @@ if (!DOCKER_IMAGE_NAME) {
   throw new Error("IMAGE_NAME is not set")
 }
 
+/**
+ * 1. Create Artifact Registry Repository
+ * This resource creates a repository in GCP Artifact Registry to store your Docker images.
+ */
 const artifactRegistryDockerImageRepo = new gcp.artifactregistry.Repository(
   "blog-backend-api-docker-image-repo",
   {
@@ -61,6 +66,10 @@ const artifactRegistryDockerImageRepo = new gcp.artifactregistry.Repository(
   },
 )
 
+/**
+ * 2. Build the Docker Image
+ * This resource builds the Docker image from the Dockerfile in the monorepo root.
+ */
 const backedApiDockerImage = new docker.Image(
   "blog-backend-image",
   {
@@ -81,6 +90,10 @@ const backedApiDockerImage = new docker.Image(
 
 const cloudRunApiName = "blog-backend-api-cloud-run"
 
+/**
+ * 3. Deploy the Docker Image to Cloud Run
+ * This resource deploys the Docker image to Cloud Run.
+ */
 const blogBackendApiService = new gcp.cloudrunv2.Service(
   cloudRunApiName,
   {
@@ -120,4 +133,11 @@ const blogBackendApiService = new gcp.cloudrunv2.Service(
   { dependsOn: backedApiDockerImage },
 )
 
+/**
+ * I have not setup any way to auto update env vars based on this URN
+ * So if you update or destroy or re-create this service you will need to
+ * manually update the env to point towards the new Backend API URL
+ *
+ * Found here: https://console.cloud.google.com/run
+ */
 export const blogBackendApiUrn = blogBackendApiService.urn
