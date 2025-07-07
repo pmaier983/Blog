ba27cc0fbde59ad86ad84c368cfb6f6f9c2c9ed6
Phillip ED Maier <pmaier983@gmail.com>
2025-07-06 19:59:24 -0700

    Get Ready for Cloudflare Pages Hosting

--- apps/astro-blog/astro.config.mjs
+++ apps/astro-blog/astro.config.mjs
@@ -1,29 +1,18 @@
-import { defineConfig, passthroughImageService } from "astro/config"
+import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
 import react from "@astrojs/react"
-import node from "@astrojs/node"
 
 import { config } from "dotenv"
-config()
 
-if (!process.env.DOMAIN) {
-  throw new Error("Please set the DOMAIN environment variable.")
-}
+config()
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  image: {
-    service: passthroughImageService(),
-  },
-  // TODO: change this to be phillipmaier.com once complete!
-  site: `https://${process.env.DOMAIN}/`,
+  site: `https://phillipmaier.com'`,
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  adapter: node({
-    mode: "standalone",
-  }),
   server: {
     host: true,
   },
